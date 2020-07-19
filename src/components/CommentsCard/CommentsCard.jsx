14af0ca9e18e41b9fcf0ef76fd0c8037b6ea28e7
acostade <acostade@gmail.com>
2020-07-19 16:44:34 -0500

    components done

--- src/components/CommentsCard/CommentsCard.jsx
+++ src/components/CommentsCard/CommentsCard.jsx
@@ -1,37 +1,36 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import userService from '../../services/userService';
 
-function CommentCard({comment, handleDeleteComment}) {
+function CommentCard({comment, handleDeleteComment, user, idx}) {
     return (
+        
         <div className='panel panel-default'>
             <div className="panel-heading">
                 <h3 className='panel-title'>{comment.name}</h3>
             </div>
             <div className='panel-body'>
                 <dl>
-                    <dt>text</dt>
-                    <dd>{comment.text}</dd>
+                    <dt>Comment</dt>
+                    <dd>{comment.name}</dd>
+                    
+                    
                 </dl>
             </div>
             <div className='panel-footer'>
-                <Link
-                    className='btn btn-xs btn-warning'
-                    to={{
-                            pathname: '/edit',
-                            state: {comment}
-                    }}
-                >
-                EDIT
-                </Link>
+                
+                {comment.owner === user.name ?
                 <button
                     className='btn btn-xs btn-danger margin-left-10'
-                    onClick={() => handleDeleteComment(comment._id)}
+                    onClick={() => handleDeleteComment(comment._id, idx)}
                 >
                     DELETE
                 </button>
+                :
+                <></>
+                }
             </div>
         </div>
     )
 }
-
 export default CommentCard;
